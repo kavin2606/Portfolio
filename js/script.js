dc06ba7c1b750338ae8671624709c46010c30e21
kavin2606 <kavin2@pdx.edu>
2019-05-19 16:59:51 -0700

    links added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+document.getElementById("db2").onclick = function () {
+location.replace("https://github.com/nivedit1/Building_Data")
+};
 
+document.getElementById("db1").onclick = function () {
+location.replace("https://github.com/kavin2606/dblp-cassandra")
+};
